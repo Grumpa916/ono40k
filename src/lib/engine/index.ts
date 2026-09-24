b3d442cd800b756377a47512f61d75c30f9b1d43
Grumpa916 <gonogono@gmail.com>
2026-09-24 13:20:13 -0700

    P2: export mission scoring evaluator

--- src/lib/engine/index.ts
+++ src/lib/engine/index.ts
@@ -4,4 +4,4 @@ export * from "./resolution";
 export * from "./integrity";
 export * from "./objective";
 export * from "./commands";
-export * from "./runtime";
+export * from "./runtime";export * from "./mission";
